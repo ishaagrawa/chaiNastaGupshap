2d795c5ad3148d96b1c6dbd386ac49da3089ac83
ishaagrawa <sweetyishamittal@gmail.com>
2025-12-31 23:35:57 +0530

    Adding my code on github

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Welcome to Chai Nasta Gupshap</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
